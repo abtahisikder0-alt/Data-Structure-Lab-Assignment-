9a846e8d606d7a1d1e550480f2c202a8fd5fddf0
abtahisikder0-alt <abtahisikder0@gmail.com>
2026-09-14 20:41:25 +0600

    Create task6.c

---
+++ task6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int findPatternIndex(char text[], char pattern[]) {
+    int n = strlen(text);
+    int m = strlen(pattern);
+
+    for (int i = 0; i <= n - m; i++) {
+        int j;
+        for (j = 0; j < m; j++) {
+            if (text[i + j] != pattern[j])
+                break;
+        }
+        if (j == m)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    char text[100], pattern[100];
+    printf("Enter Text: ");
+    scanf("%s", text);
+    printf("Enter Pattern: ");
+    scanf("%s", pattern);
+
+    int index = findPatternIndex(text, pattern);
+    if (index != -1)
+        printf("Pattern found at index: %d\n", index);
+    else
+        printf("Pattern not found.\n");
+
+    return 0;
+}
