a0be18fae34ce61c8bf2c69c7e4ac5fc3e1a82cb
abtahisikder0-alt <abtahisikder0@gmail.com>
2026-09-14 20:46:43 +0600

    Create task12.c

---
+++ task12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10, 20, 80, 30, 60, 50, 110, 100, 130, 170};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 110;
+
+    int result = linearSearch(arr, n, target);
+    if (result == -1)
+        printf("Element is not present in array\n");
+    else
+        printf("Element is present at index %d\n", result);
+    return 0;
+}
